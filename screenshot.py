7ead343a2f8fa7c932ac5aa3dba74e96bf6bf08c
Nicholas Shatalov <nicholas.shat@gmail.com>
2026-05-03 09:13:49 -0700

    Completed stuff

--- screenshot.py
+++ screenshot.py
@@ -6,7 +6,7 @@ import io
 
 # Define the folder and file name
 folder_path = "ipc_data_one"
-file_name = "current_screen.jpg"
+file_name = "current_screen.png"
 full_path = os.path.join(folder_path, file_name)
 
 scale_x = 0
@@ -17,7 +17,7 @@ def take_screenshot():
     screenshot = pyautogui.screenshot(full_path)
 
     buffer = io.BytesIO()
-    screenshot.save(buffer, format="JPEG", quality=85)
+    screenshot.save(buffer, format="PNG", quality=85)
     b64 = base64.b64encode(buffer.getvalue()).decode("utf-8")
     return b64
 
