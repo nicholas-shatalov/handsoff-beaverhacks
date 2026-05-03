c5466584fb52715e92c581a3f9c2be73b2444274
Nicholas Shatalov <nicholas.shat@gmail.com>
2026-05-03 01:11:13 -0700

    changes

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
-import brain_service_one
-import brain_service_two
 import liveJarvis
 import eyes
+import brain_service_one
+import brain_service_two
 import actions
+import ai_client
+
 
 mode = False
+
 def main():
+    # Initializes and caches models
+    ai_client.initialize()
     while True:
         if mode:
             eyes.main()
